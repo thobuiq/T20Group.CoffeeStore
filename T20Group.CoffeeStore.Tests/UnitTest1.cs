5582d1a5083f5f942a9d5ed082aee6a1b922e2a6
Bùi Quang Thọ <thobuiq@gmail.com>
2020-08-31 00:25:20 +0700

    Initial project

---
+++ T20Group.CoffeeStore.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace T20Group.CoffeeStore.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
